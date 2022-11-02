82d1f982549b5d87002e5cb1813e1c8e904fa155
Felipe <pipecalgaro@gmail.com>
2022-11-02 12:23:11 -0300

    Add send message and join room functionalities

--- main.js
+++ main.js
@@ -1,18 +1,47 @@
 import { io } from "socket.io-client";
 
+const messages = [
+  'hello',
+  'testing',
+  'first socket project',
+  'hope it works',
+  'bye'
+]
+
 document.querySelector('#app').innerHTML = `
   <div>
-    <p id="text"></p>
+    <p id="id"></p>
+    <p id="room"></p>
+    <ul id="messages"></ul>
   </div>
 `
 
 const socket = io('http://localhost:3000')
 
 socket.on('connect', () => {
-  displayMessage('connected with id ' + socket.id)
+  displayInfo('Connected with ID: ' + socket.id, 'id')
 })
 
+socket.emit('join-room', '1234')
+
+messages.map(message => socket.emit('send-message', message))
+
+socket.on('receive-message', message => {
+  displayMessage(message)
+})
+
+socket.on('joined-room', room => {
+  displayInfo('Joined room: ' + room, 'room')
+})
+
+function displayInfo(info, id) {
+  const text = document.getElementById(id)
+  text.innerHTML += info
+}
+
 function displayMessage(message) {
-  const text = document.getElementById('text')
-  text.innerHTML = message
+  const ul = document.getElementById('messages')
+  const li = document.createElement('li')
+  li.innerHTML = message
+  ul.appendChild(li)
 }
